a7b674b92b2d823a364167b5a158039f8cb7e83e
buttercup2007 <2215907@talnet.nl>
2026-09-07 11:46:06 +0200

    heb uitbrekende uitslagen toegevoegs

--- src/Controller/wedstrijdApiController.php
+++ src/Controller/wedstrijdApiController.php
@@ -3,19 +3,20 @@
 namespace App\Controller;
 
 use App\Services\WedstrijdService;
-
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
-class wedstrijdApiController 
+class wedstrijdApiController extends AbstractController
 {
 
-    #[Route('/api/wedstrijden', methods: ['GET'])]
-    public function getCollection(WedstrijdService $wedstrijdService): JsonResponse
+    #[Route('/api/wedstrijden', name: 'api_wedstrijden', methods: ['GET'])]
+    public function wedstrijden(WedstrijdService $wedstrijdService): JsonResponse
     {
-        $wedstrijden = $wedstrijdService->getWedstrijden();
-
-        return new JsonResponse($wedstrijden);
+        return $this->json([
+            'wedstrijden' => $wedstrijdService->getWedstrijden(),
+            'ontbrekendeUitslagen' => $wedstrijdService->getOntbrekendeUitslagen(),
+        ]);
 
     }
 }
--- src/Services/wedstrijdService.php
+++ src/Services/wedstrijdService.php
@@ -16,6 +16,7 @@ class WedstrijdService
         return $this->connection->fetchAllAssociative("
             SELECT
                 w.datum,
+
                 CASE
                     WHEN w.tijd LIKE '%:%' THEN w.tijd
                     WHEN LENGTH(TRIM(w.tijd)) = 4
@@ -26,13 +27,41 @@ class WedstrijdService
                         )
                     ELSE w.tijd
                 END AS tijd,
+
                 s.sportsoort AS sport,
                 w.club1nummer AS team1,
-                w.club2nummer AS team2
+                w.club2nummer AS team2,
+                w.puntenteam1,
+                w.puntenteam2
+
             FROM wedstrijd w
+
             JOIN sporten s
                 ON LEFT(w.compnummer, 3) = s.code
+
             ORDER BY w.datum, w.tijd
+        ");
+    }
+
+
+    public function getOntbrekendeUitslagen(): array
+    {
+        return $this->connection->fetchAllAssociative("
+            SELECT
+                s.sportsoort AS sport,
+                COUNT(*) AS aantal
+
+            FROM wedstrijd w
+
+            JOIN sporten s
+                ON LEFT(w.compnummer, 3) = s.code
+
+            WHERE w.puntenteam1 IS NULL
+               OR w.puntenteam2 IS NULL
+
+            GROUP BY s.sportsoort
+
+            ORDER BY aantal DESC, s.sportsoort ASC
         ");
     }
 }
