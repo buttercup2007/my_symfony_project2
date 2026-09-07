494284ef2739041639cadfecc4fba0ceabcc30f6
buttercup2007 <2215907@talnet.nl>
2026-09-07 16:07:17 +0200

    adding so that in json it works

--- src/Repository/wedstrijdRepository.php
+++ src/Repository/wedstrijdRepository.php
@@ -20,6 +20,7 @@ class WedstrijdRepository extends ServiceEntityRepository
             ->fetchAllAssociative("
                 SELECT
                     w.datum,
+
                     CASE
                         WHEN w.tijd LIKE '%:%' THEN w.tijd
                         WHEN LENGTH(TRIM(w.tijd)) = 4
@@ -30,13 +31,42 @@ class WedstrijdRepository extends ServiceEntityRepository
                             )
                         ELSE w.tijd
                     END AS tijd,
+
                     s.sportsoort AS sport,
                     w.club1nummer AS team1,
-                    w.club2nummer AS team2
+                    w.club2nummer AS team2,
+                    w.puntenteam1,
+                    w.puntenteam2
+
                 FROM wedstrijd w
+
                 JOIN sporten s
                     ON LEFT(w.compnummer, 3) = s.code
+
                 ORDER BY w.datum, w.tijd
+            ");
+    }
+
+    public function getOntbrekendeUitslagen(): array
+    {
+        return $this->getEntityManager()
+            ->getConnection()
+            ->fetchAllAssociative("
+                SELECT
+                    s.sportsoort AS sport,
+                    COUNT(*) AS aantal
+
+                FROM wedstrijd w
+
+                JOIN sporten s
+                    ON LEFT(w.compnummer, 3) = s.code
+
+                WHERE w.puntenteam1 IS NULL
+                   OR w.puntenteam2 IS NULL
+
+                GROUP BY s.sportsoort
+
+                ORDER BY aantal DESC, s.sportsoort ASC
             ");
     }
 }
--- src/Services/wedstrijdService.php
+++ src/Services/wedstrijdService.php
@@ -2,66 +2,22 @@
 
 namespace App\Services;
 
-use Doctrine\DBAL\Connection;
+use App\Repository\WedstrijdRepository;
 
 class WedstrijdService
 {
     public function __construct(
-        private Connection $connection
+        private WedstrijdRepository $wedstrijdRepository
     ) {
     }
 
     public function getWedstrijden(): array
     {
-        return $this->connection->fetchAllAssociative("
-            SELECT
-                w.datum,
-
-                CASE
-                    WHEN w.tijd LIKE '%:%' THEN w.tijd
-                    WHEN LENGTH(TRIM(w.tijd)) = 4
-                        THEN CONCAT(
-                            LEFT(TRIM(w.tijd), 2),
-                            ':',
-                            RIGHT(TRIM(w.tijd), 2)
-                        )
-                    ELSE w.tijd
-                END AS tijd,
-
-                s.sportsoort AS sport,
-                w.club1nummer AS team1,
-                w.club2nummer AS team2,
-                w.puntenteam1,
-                w.puntenteam2
-
-            FROM wedstrijd w
-
-            JOIN sporten s
-                ON LEFT(w.compnummer, 3) = s.code
-
-            ORDER BY w.datum, w.tijd
-        ");
+        return $this->wedstrijdRepository->getWedstrijden();
     }
-
 
     public function getOntbrekendeUitslagen(): array
     {
-        return $this->connection->fetchAllAssociative("
-            SELECT
-                s.sportsoort AS sport,
-                COUNT(*) AS aantal
-
-            FROM wedstrijd w
-
-            JOIN sporten s
-                ON LEFT(w.compnummer, 3) = s.code
-
-            WHERE w.puntenteam1 IS NULL
-               OR w.puntenteam2 IS NULL
-
-            GROUP BY s.sportsoort
-
-            ORDER BY aantal DESC, s.sportsoort ASC
-        ");
+        return $this->wedstrijdRepository->getOntbrekendeUitslagen();
     }
 }
