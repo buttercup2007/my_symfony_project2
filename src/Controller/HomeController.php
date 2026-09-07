12291a3b5e8c40225094c2d4df54b8b20628f6bc
buttercup2007 <2215907@talnet.nl>
2026-09-07 14:28:13 +0200

    entity wooorks

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,11 +6,22 @@ use App\Services\WedstrijdService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use App\Entity\Wedstrijd;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     #[Route('/index', name: 'home_index')]
+    #[Route('/test-entity', name: 'test_entity')]
+public function testEntity(EntityManagerInterface $entityManager): Response
+{
+    $wedstrijd = $entityManager
+        ->getRepository(Wedstrijd::class)
+        ->findOneBy([]);
+
+    dd($wedstrijd);
+}
     public function index(WedstrijdService $wedstrijdService): Response
     {
         $wedstrijden = $wedstrijdService->getWedstrijden();
