b0863ec608940445514645f04ddddd10860993d3
buttercup2007 <2215907@talnet.nl>
2026-09-10 11:31:45 +0200

    search bar works

--- src/Controller/WedstrijdController.php
+++ src/Controller/WedstrijdController.php
@@ -5,19 +5,27 @@ namespace App\Controller;
 use App\Services\WedstrijdService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 
 class WedstrijdController extends AbstractController
 {
-    #[Route('/ontbrekende-uitslagen')]
+    #[Route('/ontbrekende-uitslagen', name: 'app_wedstrijd_ontbrekende_uitslagen')]
     public function ontbrekendeUitslagen(
+        Request $request,
         WedstrijdService $wedstrijdService
+
     ): Response {
-    $wedstrijden = $wedstrijdService->getOntbrekendeUitslagen( '2026-08-29','2026-08-30');
+    $startDatum = $request->query->get('start', '2024-01-01');
+    $eindDatum = $request->query->get('eind', '2024-12-31');
     
+    $wedstrijden = $wedstrijdService->getOntbrekendeUitslagen($startDatum, $eindDatum);
+
     return $this->render('wedstrijd/ontbrekende.html.twig', [
             'wedstrijden' => $wedstrijden,
+            'startDatum' => $startDatum,
+            'eindDatum' => $eindDatum,
         ]);
     }
 }
