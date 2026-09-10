02af7ef00127dc4b50d4b0c3b6e0217747d2f542
buttercup2007 <2215907@talnet.nl>
2026-09-10 09:56:01 +0200

    ontbrekende uitslagen

--- src/Repository/wedstrijdRepository.php
+++ src/Repository/wedstrijdRepository.php
@@ -50,21 +50,32 @@ class WedstrijdRepository
         return $this->connection->fetchAllAssociative('
             SELECT
                 s.sportsoort AS sport,
-                COUNT(*) AS aantal
+                w.datum,
+
+                CASE 
+                    WHEN w.tijd LIKE \'%:%\' THEN w.tijd
+                    WHEN LENGTH(TRIM(w.tijd)) = 4
+                        THEN CONCAT(
+                            LEFT(TRIM(w.tijd), 2),
+                            \':\',
+                            RIGHT(TRIM(w.tijd), 2)
+                        )
+                    ELSE w.tijd
+                END AS tijd,
+
+                w.club1nummer AS team1,
+                w.club2nummer AS team2,
 
             FROM wedstrijd w
 
             JOIN sporten s
                 ON LEFT(w.compnummer, 3) = s.code
 
-            WHERE w.meetellen = \'J\'
-              AND (
-                    w.puntenteam1 IS NULL
-                    OR w.puntenteam2 IS NULL
-                  )
+            WHERE w.puntenteam1 IS NULL
+                OR w.puntenteam2 IS NULL
 
-            GROUP BY s.sportsoort
-            ORDER BY s.sportsoort
+            GROUP BY w.datum, w.tijd
+            
         ');
     }
 }
