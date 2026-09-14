6333825abc7266b83ff8df03a6d7cd4687f3f6af
buttercup2007 <2215907@talnet.nl>
2026-09-14 12:42:35 +0200

    weekend pagina toegevoegd

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Services\WedstrijdService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -11,10 +12,26 @@ class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
     public function index(
+        Request $request,
         WedstrijdService $wedstrijdService
     ): Response {
-    
+        $weekendOffset = $request->query->getInt('weekend', 0);
         $vandaag = new \DateTime();
+        $wedstrijden = $wedstrijdService->getWedstrijden();
+
+        $laatsteScoreDatum = null;
+        foreach ($wedstrijden as $wedstrijd) {
+            if ($wedstrijd['score1'] !== null && $wedstrijd['score2'] !== null) {
+                $scoreDatum = new \DateTime($wedstrijd['datum']);
+                if ($laatsteScoreDatum === null || $scoreDatum > $laatsteScoreDatum) {
+                    $laatsteScoreDatum = $scoreDatum;
+                }
+            }
+        }
+
+        if ($laatsteScoreDatum !== null) {
+            $vandaag = $laatsteScoreDatum;
+        }
     
         // Find the most recent Friday
         $dagVanDeWeek = (int) $vandaag->format('N');
@@ -26,6 +43,7 @@ class HomeController extends AbstractController
         }
     
         $vrijdag = (clone $vandaag)->modify("-{$dagenSindsVrijdag} days");
+        $vrijdag->modify("{$weekendOffset} weeks");
     
         // The weekend runs from Friday until Sunday
         $startDatum = (clone $vrijdag);
@@ -35,11 +53,13 @@ class HomeController extends AbstractController
             $startDatum->format('Y-m-d'),
             $eindDatum->format('Y-m-d')
         );
-    
+
         return $this->render('home/index.html.twig', [
             'overzicht' => $overzicht,
+            'wedstrijden' => $wedstrijden,
             'startDatum' => $startDatum,
             'eindDatum' => $eindDatum,
+            'weekendOffset' => $weekendOffset,
         ]);
     }
 }
--- src/Controller/WeekendController.php
+++ src/Controller/WeekendController.php
@@ -4,26 +4,70 @@ namespace App\Controller;
 
 use App\Services\WedstrijdService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class WeekendController extends AbstractController
 {
     #[Route('/weekend', name: 'app_weekend')]
-    public function index(WedstrijdService $wedstrijdService): Response
+    public function index(
+        Request $request,
+        WedstrijdService $wedstrijdService
+    ): Response
     {
-        $startDatum = '2026-09-11';
-        $eindDatum = '2026-09-13';
+        $weekendOffset = $request->query->getInt('weekend', 0);
+        $wedstrijden = $wedstrijdService->getWedstrijden();
+        $vandaag = new \DateTime();
+
+        $laatsteScoreDatum = null;
+        foreach ($wedstrijden as $wedstrijd) {
+            if ($wedstrijd['score1'] !== null && $wedstrijd['score2'] !== null) {
+                $scoreDatum = new \DateTime($wedstrijd['datum']);
+                if ($laatsteScoreDatum === null || $scoreDatum > $laatsteScoreDatum) {
+                    $laatsteScoreDatum = $scoreDatum;
+                }
+            }
+        }
+
+        if ($laatsteScoreDatum !== null) {
+            $vandaag = $laatsteScoreDatum;
+        }
+
+        $dagenSindsVrijdag = ((int) $vandaag->format('N') + 2) % 7;
+        $startDatum = (clone $vandaag)
+            ->modify("-{$dagenSindsVrijdag} days")
+            ->modify("{$weekendOffset} weeks");
+        $eindDatum = (clone $startDatum)->modify('+2 days');
+
+        $startDatumTekst = $startDatum->format('Y-m-d');
+        $eindDatumTekst = $eindDatum->format('Y-m-d');
 
         $overzicht = $wedstrijdService->getWeekendOverzicht(
-            $startDatum,
-            $eindDatum
+            $startDatumTekst,
+            $eindDatumTekst
         );
+
+        $aantalScores = 0;
+
+        foreach ($wedstrijden as $wedstrijd) {
+            if (
+                $wedstrijd['datum'] >= $startDatumTekst
+                && $wedstrijd['datum'] <= $eindDatumTekst
+                && $wedstrijd['score1'] !== null
+                && $wedstrijd['score2'] !== null
+            ) {
+                $aantalScores++;
+            }
+        }
 
         return $this->render('weekend/index.html.twig', [
             'overzicht' => $overzicht,
+            'wedstrijden' => $wedstrijden,
             'startDatum' => $startDatum,
             'eindDatum' => $eindDatum,
+            'weekendOffset' => $weekendOffset,
+            'aantalScores' => $aantalScores,
         ]);
     }
 }
